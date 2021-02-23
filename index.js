b2016870958c8363835b1e537b24b8b1fb58c27e
Sam Winterhalder <75131986+SamWinterhalder@users.noreply.github.com>
2021-02-23 03:20:44 +0000

    Dynamic commands (#2)
    * Add dynamic commands
    
    Dynamic commands are helpful as they increase readability. This is done by
    placing each command inside its own file and then searching for each command
    contained in commands folder

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const Discord = require('discord.js');
+
+const fs = require('fs');
+const {token, prefix} = require("./config.json");
+
 const bot = new Discord.Client();
-const config = require("./config.json");
+bot.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    bot.commands.set(command.name, command);
+}
+
+bot.once('ready', () => {
+	console.log('Ready');
+});
 
 bot.on('message', (message) => {
-    if(message.content == 'ping') {
-        message.channel.send('pog');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    
+    if (!bot.commands.has(command)) return;
+
+    try {
+        bot.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command.');
     }
 });
 
-bot.login(config.token);
+bot.login(token);
