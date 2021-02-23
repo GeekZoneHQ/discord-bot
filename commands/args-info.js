f267914f4bcffd68add8aa1a3abf3a14da3b94ed
Sam <winterhalder.sam@gmail.com>
2021-02-23 02:51:23 +0000

    Add dynamic commands
    Dynamic commands are helpful as they increase readability. This is done by
    placing each command inside its own file and then searching for each command
    contained in commands folder

---
+++ commands/args-info.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'args-info',
+	description: 'Command to test arguements',
+	execute(message, args) {
+		if (!args.length) {
+            return message.channel.send(`No arguements provided, ${message.author}`);
+        }
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+        
+        message.channel.send(`Command name: ${command}\nArguements: ${args}`);
+	},
+};
