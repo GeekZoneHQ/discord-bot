2aca4bb13eabe50a4d57bf250cc3c164b1d23b08
Sam <winterhalder.sam@gmail.com>
2021-02-23 14:07:18 +0000

    Schedule takes one arguement - seconds past minute

--- commands/schedule.js
+++ commands/schedule.js
@@ -4,10 +4,15 @@ module.exports = {
 	name: 'schedule',
 	description: 'Command to schedule events',
 	execute(message, args) {
-        const job = schedule.scheduleJob('25 * * * * *', function() {
-            message.channel.send('pogpogpog');
-        });
-        
+        if (!args.length) {
+            message.channel.send("No arguements passed");
+        }
+        else if (args.length === 1) {
+            const job = schedule.scheduleJob(`${args[0]} * * * * *`, function() {
+                message.channel.send(`pogpogpog${args[0]}`);
+            });
+        }
+
         message.channel.send(`Command name: ${message}\nArguements: ${args}`);
 	},
 };
