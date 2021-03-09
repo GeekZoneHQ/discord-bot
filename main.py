7e3cde1fe16aa1bfddbe8fd0da357b9a24bae810
Sam <winterhalder.sam@gmail.com>
2021-03-09 00:22:01 +0000

    Loop starts at scheduled time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
-import asyncio
 import os
 import json
+
+import asyncio
+import datetime as dt
 
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
@@ -35,21 +37,41 @@ async def reload(ctx, extension):
     client.load_extension(f'commands.{extension}')
 
 
+def get_task_start(task):
+    start_datetime = dt.datetime.strptime(
+        config[task]["start"], '%Y-%m-%d %H:%M:%S')
+    return start_datetime
+
+
+def get_task_interval(task):
+    pass
+
+
+get_task_interval("msg1")
+
+
 @tasks.loop(seconds=5)
-async def task1():
-    conf = config["task1"]
-    await client.get_channel(813465670477152266).send(conf["message"])
+async def message1():
+    conf = config["msg1"]
+    await client.get_channel(conf["channel"]).send(conf["message"])
 
 
-@task1.before_loop
+@message1.before_loop
 async def before():
+    start_time = get_task_start("msg1")
+    for _ in range(60*60*24*7):
+        if dt.datetime.now() >= start_time:
+            print('It is time')
+            print(dt.datetime.now())
+            return
+        await asyncio.sleep(1)
     await client.wait_until_ready()
 
 
 @client.event
 async def on_ready():
     print("Ready")
-    task1.start()
+    message1.start()
 
 
 for f_name in os.listdir('commands'):
